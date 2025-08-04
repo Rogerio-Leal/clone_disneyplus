17c9034922c7d2a06b73542fec0d0a61212c23c3
rogério <rogerionarcizoleal@gmail.com>
2025-08-04 20:05:33 -0300

    criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(sass())
+        .pipe(cleanCSS())
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*', {encoding: false})
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
